48550fd46bb287aeb14ae1cd191f1dfc88bf6a0e
GitoArc <142146806+GitoArc@users.noreply.github.com>
2026-02-28 06:00:00 +0100

    (most likely) final touch for Patch_TTS

--- Patch_TransitionToState.cs
+++ Patch_TransitionToState.cs
@@ -1,64 +1,48 @@
 ﻿using HarmonyLib;
-using MelonLoader;
 using SecretHistories.Entities;
+using SecretHistories.Enums;
 using SecretHistories.States;
-using SecretHistories.UI;
 
-public class RecipeSuccessEventArgs(Situation instance, SituationState newState) : EventArgs
+
+public class TransitionToStateEventArgs(Situation instance, SituationState newState)
 {
     public Situation Situation { get; } = instance;
     public SituationState SituationState { get; } = newState;
 }
 
+
 [HarmonyPatch(typeof(Situation))]
 public static class Patch_TransitionToState
 {
-    public static event Action<RecipeSuccessEventArgs> TransitionToState_Prefix = delegate { };
-    public static event Action<RecipeSuccessEventArgs> TransitionToState_Postfix = delegate { };
+    public static event Action<TransitionToStateEventArgs> StateIsRequiExec_Prefix = delegate { };
+    public static event Action<TransitionToStateEventArgs> StateIsRequiExec_Postfix = delegate { };
 
     [HarmonyPrefix]
     [HarmonyPatch(nameof(Situation.TransitionToState))]
     static void Prefix(Situation __instance, SituationState newState)
     {
-        var spheres = Watchman.Get<HornedAxe>().GetSpheres();
-        if (__instance == null) return;
-        if (newState == null) return;
+        if (__instance == null || newState == null
+            || newState.Identifier != StateEnum.RequiringExecution)
+            return;
 
-        if (__instance.GetCurrentRecipe().ActionId == "terrain.unlock")
-        {
-            int x = 0;
-        }
-        //room/terrain unlocks have Identifier == RequiresExecution tho...
-        //if (newState.Identifier == SecretHistories.Enums.StateEnum.Starting) return;
+        //var actionId = recipe.ActionId;  //eg "terrain.unlock"
+        //var id = recipe.Id;              //eg "terrain.gatehouse"
+        //var labl = recipe.Label;         //eg "Watchman\'s Tower: Gatehouse"
 
-        //if (__instance.CurrentRecipeId == "study.mastering.no") return;
-
-        Melon<Mod>.Logger.Msg($"TransitionToState.Prefix(): {__instance.Id} - {newState}");
-
-        /// award item into game
-        var r = __instance.GetCurrentRecipe();
-        //r.Effects.TryAdd("florin", "1");
-        //r.Effects.TryAdd("weather.gale", "1");
-        var actionId = r.ActionId;  //eg "terrain.unlock"
-        var id = r.Id;              //eg "terrain.gatehouse"
-        var labl = r.Label;         //eg "Watchman\'s Tower: Gatehouse"
-
-
-        TransitionToState_Prefix.Invoke(new RecipeSuccessEventArgs(__instance, newState));
+        StateIsRequiExec_Prefix.Invoke(new TransitionToStateEventArgs(__instance, newState));
     }
 
     [HarmonyPostfix]
     [HarmonyPatch(nameof(Situation.TransitionToState))]
     static void Postfix(Situation __instance, SituationState newState)
     {
-        if (__instance?.CurrentRecipeId == "ap.recipe.reward") {
-            //scrub the awarded ap-items (so they dont get repeatedly awarded)
-            var r = __instance.GetCurrentRecipe();
-            r.Effects = [];
-            r.Notable = false;
-            ///just to be safe it is applied:
-            __instance.SetCurrentRecipe(r);
-        }
-        TransitionToState_Postfix.Invoke(new RecipeSuccessEventArgs(__instance, newState));
+        if (__instance == null || newState == null
+            || newState.Identifier != StateEnum.RequiringExecution)
+            return;
+        // 'StateEnum.RequiringExecution' seems to act as a 'finalizer'?
+        //  Some situations  (like looping recipes or terrain.unlocks)  never get 'StateEnum.Complete'
+        //  but 'StateEnum.RequiringExecution' always happens (at least so far)
+
+        StateIsRequiExec_Postfix.Invoke(new TransitionToStateEventArgs(__instance, newState));
     }
 }
