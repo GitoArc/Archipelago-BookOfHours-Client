fde1da1d643b7b3892a380b11b3ffb04a2d3c19a
GitoArc <142146806+GitoArc@users.noreply.github.com>
2026-03-01 10:19:47 +0100

    finalised the WisdomTree hooks

--- Patch_WisdomNodeTerrain.cs
+++ Patch_WisdomNodeTerrain.cs
@@ -1,22 +1,54 @@
 using HarmonyLib;
 using MelonLoader;
-using SecretHistories.Entities;
-using SecretHistories.Spheres;
+using SecretHistories.Enums;
+using SecretHistories.Events;
 using SecretHistories.Tokens.Payloads;
-using SecretHistories.UI;
-using UnityEngine.InputSystem.LowLevel;
+using System.ComponentModel;
+
 
 [HarmonyPatch(typeof(WisdomNodeTerrain))]
 public static class Patch_WisdomNodeTerrain
 {
-    [HarmonyPostfix]
-    [HarmonyPatch(nameof(WisdomNodeTerrain.TryCommitToken))]
-    static void Postfix(WisdomNodeTerrain __instance, Token token)
-    {
-        //bc the original method doesnt return antyhing, validating is bs
-        //just assume it locked in?
+    public static event Action<string> WisdomNodeCommitted = delegate { };
+    public static event Action<string> OutputTokenSpawned = delegate { };
 
-        Melon<Mod>.Logger.Msg($"'{token.Payload.Label}' has been assigned to the Tree: '{__instance.Id}'");
-        //this.CommitToken(token2, firstMatchingRecipe);
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(WisdomNodeTerrain.OnTokensChangedForSphere))]
+    static void OnTokensChangedForSphere_Postfix(WisdomNodeTerrain __instance, SphereContentsChangedEventArgs args)
+    {
+        // ! this will fire when you load a save - for every committed 
+
+        // args.Change == TokenAdded --> A card was put into an empty slot... - this by itself is USELESS, since user can pull it back out
+        //      ... OR SPHERE : There exists a "commitment" sphere - which only fires when the Token is CONFIRMED LOCKED in <- VERY NOT USELESS
+
+        // args.Change == TokenChanged -->
+        //      happens MULTIPLE TIMES when committing (bc the aspect-mutations? : "Committed" and "Attuned: xyz" get added, one Path-Aspect gets removed)
+        //      OR when the "face-down card" gets spawned - I guess the card gets added in the normal/"face-up" state, and this get called because Veiling/Unveiling ("flipping") the card counts for TokenChange?
+        //      OR when the "face-down card" gets clicked - this unveils the card; See line above ^
+
+        // args.Change == TokenRemoved --> when the card/token gets removed from a sphere
+        //      kinda useless bc I can determine everything I need from TokenAdded/TokenChanged
+
+#if DEBUG
+        string msg = args.Change switch
+        {
+            SphereContentChange.TokenAdded => $"TokenAdded - {args.Token.PayloadEntityId} - {args.Sphere}",
+            SphereContentChange.TokenRemoved => $"TokenRemoved - {args.Token.PayloadEntityId} - {args.Sphere}",
+            SphereContentChange.TokenChanged => $"TokenChanged - {args.Token.PayloadEntityId} - {args.Sphere}",
+            _ => throw new InvalidEnumArgumentException(),
+        };
+        Melon<Mod>.Logger.Msg(msg);
+#endif
+
+        if (args.Change == SphereContentChange.TokenAdded
+            && args.Sphere.name == "commitment" && args.Sphere.SphereCategory == SphereCategory.Commitment)
+        {
+            WisdomNodeCommitted.Invoke(__instance.Id);
+        }
+        else if (args.Change == SphereContentChange.TokenAdded
+            && args.Sphere.name == "output" && args.Sphere.SphereCategory == SphereCategory.Threshold)
+        {
+            OutputTokenSpawned.Invoke(args.Token.Payload.Label);
+        }
     }
 }
